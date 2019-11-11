725b7b4c98f124a116b2a3312db4d85bc51f45a6
Logynymo <mathiasry1@gmail.com>
2019-11-11 13:10:39 +0100

    Add files via upload

---
+++ GoldDigger/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BIZ;
+
+namespace GoldDigger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ CB;
+        UserControlUpdateMetalPrice userControlUpdateMetalPrice;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CB = new ClassBIZ();
+            userControlUpdateMetalPrice = new UserControlUpdateMetalPrice(CB);
+            SubGridMetalPrice.Children.Add(userControlUpdateMetalPrice);
+        }
+    }
+}
---
+++ Repository/ClassDollarRate.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Repository
+{
+    public class ClassDollarRate : ClassPreciusMetals
+    {
+        private int _Id;
+        private double _Rate;
+        private DateTime _RateDate;
+
+        public ClassDollarRate()
+        {
+
+        }
+
+        public int Id
+        {
+            get { return _Id; }
+            set
+            {
+                if (value != _Id)
+                {
+                    _Id = value;
+                    Notify("Id");
+                }
+            }
+        }
+
+        public double Rate
+        {
+            get { return _Rate; }
+            set
+            {
+                if (value != _Rate)
+                {
+                    _Rate = value;
+                    Notify("Rate");
+                }
+            }
+        }
+
+        public DateTime RateDate
+        {
+            get { return _RateDate; }
+            set
+            {
+                if (value != _RateDate)
+                {
+                    _RateDate = value;
+                    Notify("RateDate");
+                }
+            }
+        }
+    }
+}
